3eda0ce021c39cd7fbfe824c697cf7f07f2dfe59
unknown <pratiktikande12@gmail.com>
2023-07-04 21:16:33 +0530

    04-07-2023

---
+++ DataTypes.java
@@ -0,0 +1,32 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+        // Non-primitive data types: The non-primitive data types include Classes, Interfaces, and Arrays.
+
+// *        There are 8 types of primitive data types:
+
+
+// boolean data type
+// byte data type
+// char data type
+// short data type
+// int data type
+// long data type
+// float data type
+// double data type ** //
+
+
+// Data Type	Default Value	Default size
+// boolean	       false	1 bit
+// char	'        \u0000'	2 byte
+// byte         	0	1 byte
+// short        	0	2 byte
+// int	            0	4 byte
+// long         	0L	8 byte
+// float	        0.0f	4 byte
+// double	        0.0d	8 byte
+
+System.out.println("Data Types Present In The Comments Read It");
+    }
+}
---
+++ DataTypes2.java
@@ -0,0 +1,27 @@
+public class DataTypes2 {
+    public static void main(String[] args) {
+        System.out.println("Lets Understand Data Types");
+    
+        int a= 10;
+        System.out.println(a);
+
+       // Boolean store true or false value
+        System.out.println("boolean Size is 1bit");
+
+        byte c=10;
+        System.out.println(c +" 1 Byte");
+
+        Short d=5;
+        System.out.println(d +" 2 byte");
+
+        long e=10;
+        System.out.println(e+" 8 byte");
+
+        float f=1;
+        System.out.println(f +" 4 byte");
+
+        double g =10;
+        System.out.println(g +" 8 byte");
+
+    }
+}
