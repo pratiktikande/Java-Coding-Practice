04b87b33522169d03c977bcbd77c88cfbea0cdb2
unknown <pratiktikande12@gmail.com>
2023-07-11 23:22:16 +0530

    11-07-2023

---
+++ July_2023/leapyear.java
@@ -0,0 +1,30 @@
+package July_2023;
+
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scanner.close();
+    }
+    
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
