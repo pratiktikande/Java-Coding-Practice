29e9be96c75b3f296a40a939e20fbeeb56e013ad
unknown <pratiktikande12@gmail.com>
2023-07-12 23:10:31 +0530

    12-07-2023

---
+++ July_2023/Userinputfunction.java
@@ -0,0 +1,17 @@
+package July_2023;
+import java.util.Scanner;
+
+
+public class Userinputfunction {
+    public static void Userinputadd(int a,int b){
+        System.out.println("The Sum Of Two Numbers Is " + (a+b));
+    }
+    public static void main(String[] args) {
+          System.out.println("Enter The Two Numbers");
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b =sc.nextInt();
+        Userinputadd(a,b);
+        sc.close();
+    }
+}
---
+++ July_2023/addfuction.java
@@ -0,0 +1,14 @@
+package July_2023;
+
+public class addfuction {
+
+    //Function Created
+    public static void sum(int a,int b){
+        System.out.println("The Sum of TWO Numbers IS " + (a+b));
+    }
+
+    public static void main(String[] args) {
+        //Function call
+        sum(4,6);
+    }
+}
