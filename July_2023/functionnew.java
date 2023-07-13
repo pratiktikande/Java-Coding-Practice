a0c2b3911cbd2618e2db98ee4c524249a0ae9d12
unknown <pratiktikande12@gmail.com>
2023-07-13 16:17:47 +0530

    13-07-2023

---
+++ July_2023/functionnew.java
@@ -0,0 +1,39 @@
+package July_2023;
+
+import java.util.Scanner;
+
+public class functionnew {
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+
+    }
+
+    public static void product(int a, int b) {
+        System.out.println(a * b);
+
+    }
+
+    public static void divide(int a, int b) {
+        System.out.println(a / b);
+
+    }
+
+    public static void minus(int a, int b) {
+        System.out.println(a - b);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Number For +,-,*,/ ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        sum(a, b);
+        product(a, b);
+        minus(a, b);
+        divide(a, b);
+
+    }
+}
