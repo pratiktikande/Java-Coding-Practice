8358790ba6ccff868cfa9d1a2b743a383b020706
unknown <pratiktikande12@gmail.com>
2023-07-17 21:02:15 +0530

    17-07-2023

---
+++ July_2023/LargestAmoung3_2.java
@@ -0,0 +1,21 @@
+package July_2023;
+
+public class LargestAmoung3_2 {
+    // Using the if-else statements
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 40;
+        if (a > b & a > c) {
+            System.out.println(a + " The Largest Number");
+        } else if (b > a & b > c) {
+            System.out.println(b + " The Largest Number");
+
+        } else if (c > a & c > b) {
+            System.out.println(c + " The Largest Number");
+
+        }
+
+    }
+}
