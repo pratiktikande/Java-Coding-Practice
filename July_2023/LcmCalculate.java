34fb1d9e8ee72c4695ea6ef8939316b9f7338ec7
unknown <pratiktikande12@gmail.com>
2023-07-18 23:25:20 +0530

    18-07-2023

---
+++ July_2023/LcmCalculate.java
@@ -0,0 +1,21 @@
+package July_2023;
+public class LcmCalculate {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 18;
+        int lcm = findLCM(num1, num2);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+    }
+
+    public static int findLCM(int num1, int num2) {
+        int lcm = Math.max(num1, num2); // Start with the larger number
+
+        for (int i = lcm; ; i += lcm) {
+            if (i % num1 == 0 && i % num2 == 0) {
+                lcm = i; // Update lcm with the found value
+                break; // LCM found
+            }
+        }
+        return lcm;
+    }
+}
