59cdb6a4bfbb22d28cb27cddcc8164da8ca343fe
unknown <pratiktikande12@gmail.com>
2023-07-21 18:22:59 +0530

    21-07-2023

--- DSA/Recursion/FibonacciNumbers.java
+++ DSA/Recursion/FibonacciNumbers.java
@@ -2,11 +2,26 @@ package DSA.Recursion;
 
 public class FibonacciNumbers {
 
-    public static void CalcuFibo(){
-        
+    public static int CalcuFibo(int n) {
+        // Base Case
+        if (n == 0 || n == 1) {
+            return n;
+        }
 
+        // Recursion function
+        int fnm1 = CalcuFibo(n - 1);
+        int fnm2 = CalcuFibo(n - 2);
+        // Fibonacci Number Formula
+        int fn = fnm1 + fnm2;
+        return fn;
     }
+
     public static void main(String[] args) {
-        
+        int n = 5;
+        System.out.println(CalcuFibo(n));
+        System.out.println(CalcuFibo(23));
+        System.out.println(CalcuFibo(24));
+        System.out.println(CalcuFibo(25));
+
     }
 }
