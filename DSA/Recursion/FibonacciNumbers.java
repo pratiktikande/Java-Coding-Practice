8daab60a27caef105e0a8d18a3a153ac28fb042a
unknown <pratiktikande12@gmail.com>
2023-07-22 20:29:27 +0530

    22-07-2023

--- DSA/Recursion/FibonacciNumbers.java
+++ DSA/Recursion/FibonacciNumbers.java
@@ -7,21 +7,19 @@ public class FibonacciNumbers {
         if (n == 0 || n == 1) {
             return n;
         }
-
-        // Recursion function
+        // Two Recursion function Call
         int fnm1 = CalcuFibo(n - 1);
         int fnm2 = CalcuFibo(n - 2);
         // Fibonacci Number Formula
         int fn = fnm1 + fnm2;
         return fn;
     }
-
     public static void main(String[] args) {
         int n = 5;
         System.out.println(CalcuFibo(n));
         System.out.println(CalcuFibo(23));
         System.out.println(CalcuFibo(24));
         System.out.println(CalcuFibo(25));
-
     }
 }
+  
