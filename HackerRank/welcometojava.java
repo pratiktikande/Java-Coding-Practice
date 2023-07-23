d5094dcca25f644403a4dbf30b97354a51e9dccd
unknown <pratiktikande12@gmail.com>
2023-07-23 23:26:36 +0530

    23-07-2023

--- HackerRank/welcometojava.java
+++ HackerRank/welcometojava.java
@@ -1,6 +1,6 @@
  package HackerRank;
  
-public class welcometojava {
+public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
