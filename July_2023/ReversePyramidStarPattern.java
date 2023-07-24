96c3bbefcb4656f5876d6ae6aa3692499f391262
unknown <pratiktikande12@gmail.com>
2023-07-24 13:32:30 +0530

    24-07-2023

---
+++ July_2023/ReversePyramidStarPattern.java
@@ -0,0 +1,30 @@
+package July_2023;
+import java.util.Scanner;
+
+public class ReversePyramidStarPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of number: ");
+        int number = scanner.nextInt();
+        
+        printReversePyramidStarPattern(number);
+        scanner.close();
+    }
+
+    public static void printReversePyramidStarPattern(int number) {
+        for (int i = 1; i <= number; i++) {
+            // Print spaces
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= 2 * (number - i) + 1; k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line after each row is printed
+            System.out.println();
+        }
+    }
+}
