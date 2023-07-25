da0b965409c7c4727f1b6ad8bdad2653c15ad349
unknown <pratiktikande12@gmail.com>
2023-07-25 23:14:15 +0530

    25-07-2023

---
+++ July_2023/MirrorUpperStarTrianglePattern.java
@@ -0,0 +1,26 @@
+package July_2023;
+
+import java.util.Scanner;
+
+public class MirrorUpperStarTrianglePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the triangle: ");
+        int rows = scanner.nextInt();
+        scanner.close();
+
+        for (int i = rows; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
