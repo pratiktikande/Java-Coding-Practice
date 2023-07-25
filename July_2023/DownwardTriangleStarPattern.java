1430f91056e2885d8cde2a3cd9e54599110ad179
unknown <pratiktikande12@gmail.com>
2023-07-25 23:13:14 +0530

    25-07-2023

---
+++ July_2023/DownwardTriangleStarPattern.java
@@ -0,0 +1,22 @@
+package July_2023;
+
+import java.util.Scanner;
+
+public class DownwardTriangleStarPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the triangle: ");
+        int rows = scanner.nextInt();
+        scanner.close();
+
+        for (int i = rows; i >= 1; i--) {
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
