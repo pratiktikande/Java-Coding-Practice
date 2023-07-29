d3e0e4d8349d0a3d8c3b63fb28e219a12487d507
unknown <pratiktikande12@gmail.com>
2023-07-29 23:18:21 +0530

    29-07-2023

---
+++ Conversions/DecimalToBinary.java
@@ -0,0 +1,31 @@
+package Conversions;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int decimalNumber = scanner.nextInt();
+        scanner.close();
+
+        String binary = decimalToBinary(decimalNumber);
+        System.out.println("Binary representation: " + binary);
+    }
+
+    public static String decimalToBinary(int decimalNumber) {
+        if (decimalNumber == 0) {
+            return "0";
+        }
+
+        StringBuilder binaryBuilder = new StringBuilder();
+
+        while (decimalNumber > 0) {
+            int remainder = decimalNumber % 2;
+            binaryBuilder.insert(0, remainder);
+            decimalNumber /= 2;
+        }
+
+        return binaryBuilder.toString();
+    }
+}
