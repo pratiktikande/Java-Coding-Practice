c763a47e1d8ae9860dc2809a30cd39f1fc4c9b39
unknown <pratiktikande12@gmail.com>
2023-07-30 20:37:19 +0530

    30-07-2023

---
+++ Conversions/CharToIntConversionExample.java
@@ -0,0 +1,16 @@
+package Conversions;
+
+public class CharToIntConversionExample {
+    public static void main(String[] args) {
+        char charValue = 'A';
+        int intValue = (int) charValue;
+        System.out.println("Converted int value: " + intValue);
+        
+        //and
+
+        char digitChar = '5';
+        int numericValue = Character.getNumericValue(digitChar);
+        System.out.println("Numeric value of '" + digitChar + "': " + numericValue);
+
+    }
+}
---
+++ Conversions/IntToCharConversionExample.java
@@ -0,0 +1,10 @@
+package Conversions;
+
+public class IntToCharConversionExample {
+    public static void main(String[] args) {
+        int intValue = 65; // ASCII value for 'A'
+        char charValue = (char) intValue;
+        System.out.println("Converted char value: " + charValue);
+    }
+}
+
