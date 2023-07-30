99c1c0d0d53a36fc22b0bc6dc00487d18ec2d476
unknown <pratiktikande12@gmail.com>
2023-07-30 19:54:30 +0530

    30-07-2023

---
+++ Conversions/DoubleToStringExample1.java
@@ -0,0 +1,10 @@
+package Conversions;
+
+public class DoubleToStringExample1 {
+    //Method 2: Using String.valueOf(double)
+    public static void main(String[] args) {
+        double number = 123.45;
+        String convertedString = String.valueOf(number);
+        System.out.println("Converted String: " + convertedString);
+    }
+}
