b173f4bc659875d8ea84fc4177b272da05bacfcf
unknown <pratiktikande12@gmail.com>
2023-07-30 20:54:36 +0530

    30-07-2023

---
+++ July_2023/MultiplyFloatNumbers.java
@@ -0,0 +1,20 @@
+package July_2023;
+
+import java.util.Scanner;
+
+public class MultiplyFloatNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first floating-point number: ");
+        float num1 = scanner.nextFloat();
+
+        System.out.print("Enter the second floating-point number: ");
+        float num2 = scanner.nextFloat();
+
+        float result = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + result);
+
+        scanner.close();
+    }
+}
