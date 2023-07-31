3894b21bd1d58df09c453faa3b28a4812a7bae8d
unknown <pratiktikande12@gmail.com>
2023-07-31 20:58:59 +0530

    31-07-2023

---
+++ DSA/Recursion/CheckArraySort.java
@@ -0,0 +1,25 @@
+package DSA.Recursion;
+public class CheckArraySort {
+
+public static boolean isSorted(int arr[], int i)
+{
+     //base case
+    if(i == arr.length-1){
+        return true;
+    }
+
+    //Recursion 
+    if(arr[i] > arr[i+1]){
+        return false;
+    }
+    return isSorted(arr , i+1);
+}
+
+public static void main(String[] args) {
+    int arr[]={1,2,3,4};
+    int arr1[]={2,6,5,7};
+    System.out.println(isSorted(arr, 0));
+    System.out.println(isSorted(arr1, 0));
+}
+}
+
