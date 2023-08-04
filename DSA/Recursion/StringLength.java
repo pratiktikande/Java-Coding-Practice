7c4a665de03efb83557ca35af4eb3c91516201d7
unknown <pratiktikande12@gmail.com>
2023-08-04 23:17:20 +0530

    04-08-2023

---
+++ DSA/Recursion/StringLength.java
@@ -0,0 +1,18 @@
+package DSA.Recursion;
+
+
+public class StringLength {
+    public static int length(String str){
+        //Base Case
+        if(str.length()==0){
+            return 0;
+        }
+        //Recursion Function
+        return length(str.substring(1)) +1;
+   }
+
+   public static void main(String[] args) {
+    String str = "abcdes";
+    System.out.println(length(str));
+   }
+}
