20d950bb970755f222861f0dea838b8a837e511a
unknown <pratiktikande12@gmail.com>
2023-08-06 01:07:25 +0530

    Random Number Generate

---
+++ July_2023/RandomNumberGenrate2.java
@@ -0,0 +1,15 @@
+package July_2023;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomNumberGenrate2 {
+    public static void main(String[] args) {
+        // Generate a random integer between 0 (inclusive) and 100 (exclusive)
+        int randomNumber = ThreadLocalRandom.current().nextInt(100);
+        System.out.println("Random Number: " + randomNumber);
+        
+        // Generate a random double between 0 (inclusive) and 1 (exclusive)
+        double randomDouble = ThreadLocalRandom.current().nextDouble();
+        System.out.println("Random Double: " + randomDouble);
+    }
+}
