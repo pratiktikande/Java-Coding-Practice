ebf88a9c1b4bb4e73ffea305b7f2abca8aab8d6e
unknown <pratiktikande12@gmail.com>
2023-08-06 01:06:53 +0530

    Random Number Generate

---
+++ July_2023/RandomNumberGenerator.java
@@ -0,0 +1,17 @@
+package July_2023;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        // Generate a random integer between 0 (inclusive) and 100 (exclusive)
+        int randomNumber = random.nextInt(100);
+        System.out.println("Random Number: " + randomNumber);
+        
+        // Generate a random double between 0 (inclusive) and 1 (exclusive)
+        double randomDouble = random.nextDouble();
+        System.out.println("Random Double: " + randomDouble);
+    }
+}
