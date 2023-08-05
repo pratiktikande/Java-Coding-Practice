069431a8047b943987df23a43e5b5a932f26eb0d
unknown <pratiktikande12@gmail.com>
2023-08-05 17:52:13 +0530

    05-08-2023

---
+++ DSA/Recursion/BinaryString.java
@@ -0,0 +1,22 @@
+package DSA.Recursion;
+
+public class BinaryString {
+    //Paytm Intervivew Que
+
+    public static void printbinaryStrings(int n, int lastplace , String str){
+        //base case
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+
+        //Recursion function
+        printbinaryStrings(n-1, 0, str+"0");
+        if(lastplace == 0){
+            printbinaryStrings(n-1, 1, str+"1");
+        }
+    }
+    public static void main(String[] args) {
+        printbinaryStrings(3, 0,"");
+    }
+}
---
+++ DSA/Recursion/FriendsPairingProblem.java
@@ -0,0 +1,29 @@
+package DSA.Recursion;
+ public class FriendsPairingProblem {
+
+    public static int friendsPairing(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+
+        //chocice 
+        //single
+        int fnm1 = friendsPairing(n-1);
+
+        //pair
+        int fnm2 = friendsPairing(n-2);
+        int pairWays = (n-1) * fnm2;
+
+        //toWays
+        int totalWays = fnm1 + pairWays;
+        return totalWays;
+
+        //One Line statement
+       // return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
+ 
+    public static void main(String[] args) {
+        System.out.println(friendsPairing(3));
+        
+    }
+ }
