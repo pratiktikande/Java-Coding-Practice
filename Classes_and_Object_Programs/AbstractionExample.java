2f0ea55bb0b72a742b34e51e8d5e6a5a6d6740cb
unknown <pratiktikande12@gmail.com>
2023-08-08 23:00:17 +0530

    08-08-2023

---
+++ Classes_and_Object_Programs/AbstractionExample.java
@@ -0,0 +1,47 @@
+package Classes_and_Object_Programs;
+
+// Abstract class
+abstract class Shape {
+    // Abstract method without a body
+    abstract double calculateArea();
+}
+
+// Concrete subclass of Shape
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Concrete subclass of Shape
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return width * height;
+    }
+}
+
+public class AbstractionExample {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+    }
+}
---
+++ Classes_and_Object_Programs/MethodOverloadingExample.java
@@ -0,0 +1,30 @@
+package Classes_and_Object_Programs;
+
+public class MethodOverloadingExample {
+
+    // Method with one integer parameter
+    public void printNumber(int num) {
+        System.out.println("Integer: " + num);
+    }
+
+    // Method with one double parameter
+    public void printNumber(double num) {
+        System.out.println("Double: " + num);
+    }
+
+    // Method with two integer parameters
+    public void printSum(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+        MethodOverloadingExample example = new MethodOverloadingExample();
+
+        example.printNumber(10);       // Calls the first printNumber method
+        example.printNumber(3.14);     // Calls the second printNumber method
+
+        example.printSum(5, 7);        // Calls the printSum method
+    }
+}
+
