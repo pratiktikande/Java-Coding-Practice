e93b17edf9a6512b28b8169caaf196b27d62e473
unknown <pratiktikande12@gmail.com>
2023-08-09 23:18:34 +0530

    09-08-2023

---
+++ July_2023/AnotherHardestPatternEver.java
@@ -0,0 +1,25 @@
+package July_2023;
+
+public class AnotherHardestPatternEver {
+    public static void main(String[] args) {
+        int size = 7;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size * 4; j++) {
+                if (j <= i) {
+                    System.out.print(j);
+                } else if (j <= size * 2 - i) {
+                    System.out.print(" ");
+                } else if (j <= size * 2) {
+                    System.out.print(size * 2 - j + 1);
+                } else if (j <= size * 2 + i) {
+                    System.out.print(" ");
+                } else if (j <= size * 4 - i + 1) {
+                    System.out.print(j - size * 2);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
