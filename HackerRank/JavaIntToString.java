c21eb07d2a92ab18adc0f28d2423840c347448d9
unknown <pratiktikande12@gmail.com>
2023-08-14 00:24:59 +0530

    JavaIntToString

---
+++ HackerRank/JavaIntToString.java
@@ -0,0 +1,20 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaIntToString {
+    public static void main(String[] args) {
+           Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            //String s=???; Complete this line below
+            String s = String.valueOf(n);
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        }
+    }
+    
