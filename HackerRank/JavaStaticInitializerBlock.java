942acf0fee913bdab1756dfc6cd12db6307fd50a
unknown <pratiktikande12@gmail.com>
2023-08-14 00:02:29 +0530

    14-08-2023

---
+++ HackerRank/JavaStaticInitializerBlock.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    public static void Para(int base, int height){
+        if(base> 0 && height>0){
+            System.out.println(base*height);
+        }else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int height =sc.nextInt();
+        sc.close();
+        Para(base, height);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+}
+    
+}
