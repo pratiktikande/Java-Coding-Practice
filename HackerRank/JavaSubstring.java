b72e5ab0863022f852bc007105835532d66e1fc4
unknown <pratiktikande12@gmail.com>
2023-08-15 00:09:25 +0530

    15-08-2023

---
+++ HackerRank/JavaSubstring.java
@@ -0,0 +1,15 @@
+package HackerRank;
+
+import java.util.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        in.close();
+        System.out.println(str.substring(start, end));
+    }
+}
