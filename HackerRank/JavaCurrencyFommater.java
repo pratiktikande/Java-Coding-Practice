5c0a9296219cd79eeb6ee0178417f9513d88b0f6
unknown <pratiktikande12@gmail.com>
2023-08-14 23:34:28 +0530

    14-08-2023

---
+++ HackerRank/JavaCurrencyFommater.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFommater {
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat in = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat ch = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat fr = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + in.format(payment));
+        System.out.println("China: " + ch.format(payment));
+        System.out.println("France: " + fr.format(payment));
+    }
+    }
