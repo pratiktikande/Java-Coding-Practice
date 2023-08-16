a6e88783ba549b3957d6523d0c00242ece472bec
unknown <pratiktikande12@gmail.com>
2023-08-16 22:55:08 +0530

    16-08-2023

---
+++ HackerRank/JavaStringReverse.java
@@ -0,0 +1,14 @@
+package HackerRank;
+
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(str.equals(reversed) ? "Yes" : "No");
+    }
+}
