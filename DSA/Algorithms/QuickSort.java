ff5efe7bfece37f1401e070f5d20d743685f235f
unknown <pratiktikande12@gmail.com>
2023-08-18 18:06:53 +0530

    Divide And Conqure

---
+++ DSA/Algorithms/QuickSort.java
@@ -0,0 +1,53 @@
+package DSA.Algorithms;
+
+public class QuickSort {
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ "");
+        }
+        System.out.println();
+    }
+
+    public static void quciksort(int arr[], int si , int ei){
+        //recursion function
+        //base case
+        if(si>=ei){
+            return;
+        }
+
+        //partition function
+        int pIndex= partition(arr , si , ei);
+         //To solve left side
+         quciksort(arr, si, pIndex-1);
+         //To Solve Right Side of the array
+         quciksort(arr, pIndex+1, ei);
+    }
+
+    public static int partition(int arr[], int si , int ei){
+        int pivot = arr[ei];
+        //To make space 
+        int i= si-1;
+
+        for(int j=si; j<ei; j++){
+            if(arr[j]<=pivot){
+                //swap the values
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        //To Return The Pivot At correct position
+        i++;
+        int temp = pivot;
+        arr[ei]=arr[i];
+        arr[i]= temp;
+        return i;
+    }
+    public static void main(String[] args) {
+        int arr[]={6,3,9,8,2,};
+        quciksort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+}
---
+++ DSA/Algorithms/RotatedSortedArray.java
@@ -0,0 +1,46 @@
+package DSA.Algorithms;
+
+public class RotatedSortedArray {
+
+    // Modified Binary search
+    public static int search(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        // To find mid
+        int mid = (si + ei) / 2;
+
+        // Compare With Mid
+        if (target == arr[mid]) {
+            return mid;
+        }
+
+        // First condition found on line one
+        if (arr[si] <= arr[mid]) {
+            // Search In Left Direction
+            if (arr[si] <= target && target <= arr[mid]) {
+                // Recursion call
+                return search(arr, target, si, mid - 1);
+            } else {
+                // Search on Right side
+                return search(arr, target, mid + 1, ei);
+            }
+        } else {
+            // Search On right side
+            if (arr[mid] <= target && target <= arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            } else {
+                // Search on left side
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0; // output should be index 4 of arr
+        int targetIndex = search(arr, target, 0, arr.length - 1);
+        System.out.println(targetIndex);
+    }
+}
