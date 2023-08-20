2d692050d24de794f1d30a6e718a7e687cbd48c8
unknown <pratiktikande12@gmail.com>
2023-08-21 00:30:55 +0530

    Passed All Test Cases

--- HackerRank/JavaStringTokens.java
+++ HackerRank/JavaStringTokens.java
@@ -1,19 +1,28 @@
 package HackerRank;
+
 import java.util.*;
 
 public class JavaStringTokens {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        String s = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        
+        // Read the input string from the user and remove leading/trailing spaces
+        String s = scanner.nextLine().trim();
         scanner.close();
         
-        // Split the string into tokens using the specified regex
+        // If the input string is empty, there are no tokens
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        
+        // Split the input string into tokens using the given regular expression
+        // The regular expression "[ !,?._'@]+" matches any combination of the specified characters
         String[] tokens = s.split("[ !,?._'@]+");
         
         // Print the number of tokens
         System.out.println(tokens.length);
-           
+        
         // Print each token on a new line
         for (String token : tokens) {
             System.out.println(token);
