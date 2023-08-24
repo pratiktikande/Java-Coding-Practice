e1cc3e97a7146db8849f67527fd5973755800364
unknown <pratiktikande12@gmail.com>
2023-08-25 00:11:33 +0530

    25-08-2023

--- Interview_Prep/SwapNumbers.java
+++ Interview_Prep/SwapNumbers.java
@@ -1,2 +1,28 @@
 package Interview_Prep;
 
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Original numbers: a = " + a + ", b = " + b);
+
+        // Using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping using temporary variable: a = " + a + ", b = " + b);
+
+        // Using addition and subtraction
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping using addition and subtraction: a = " + a + ", b = " + b);
+
+        // Using XOR bitwise operation
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping using XOR bitwise operation: a = " + a + ", b = " + b);
+    }
+}
