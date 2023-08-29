fe93d93df030fdb54cb3713b0b0147c235e96ea1
unknown <pratiktikande12@gmail.com>
2023-08-29 23:09:34 +0530

    Optimize Solution

--- TCS/Numbers/ClimbStairs.java
+++ TCS/Numbers/ClimbStairs.java
@@ -24,3 +24,31 @@ package TCS.Numbers;
         System.out.println("Distinct ways: " + solution.climbStairs(n));
     }
 }
+
+
+//Optimize Solution 
+
+// public class Solution {
+//     public int climbStairs(int n) {
+//         if (n <= 2) {
+//             return n;
+//         }
+        
+//         int prev1 = 1;
+//         int prev2 = 2;
+        
+//         for (int i = 3; i <= n; i++) {
+//             int current = prev1 + prev2;
+//             prev1 = prev2;
+//             prev2 = current;
+//         }
+        
+//         return prev2;
+//     }
+
+//     public static void main(String[] args) {
+//         Solution solution = new Solution();
+//         int n = 4; // Replace with the desired number of steps
+//         System.out.println("Distinct ways: " + solution.climbStairs(n));
+//     }
+// }
