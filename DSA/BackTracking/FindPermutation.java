d3216658de68e61a725b6bb9617ab6a277807439
unknown <pratiktikande12@gmail.com>
2023-08-30 16:33:36 +0530

    String - Permutation

---
+++ DSA/BackTracking/FindPermutation.java
@@ -0,0 +1,27 @@
+package DSA.BackTracking;
+
+public class FindPermutation {
+
+    //TC = O(n* n!)
+
+    public static void findpermutation(String str, String ans){
+        //Base Case
+        if(str.length() ==0){
+            System.out.println(ans);
+            return;
+        }
+        //Recursion
+        for(int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            //"Abcd" = "a" + "cd" = acd
+          String  Newstr = str.substring(0, i)+str.substring(i+1);
+            findpermutation(Newstr, ans+curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str ="abc";
+        findpermutation(str, "");
+    }
+    
+}
