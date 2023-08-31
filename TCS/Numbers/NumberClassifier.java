f2bbbb9abf350ef8f0ace67bc8aaf1be564616c1
unknown <pratiktikande12@gmail.com>
2023-08-31 21:17:51 +0530

    TCS-Numbers

---
+++ TCS/Numbers/NumberClassifier.java
@@ -0,0 +1,31 @@
+package TCS.Numbers;
+ //Program to check if a number is Positive, Negative, Odd, Even, Zero 
+
+ import java.util.Scanner;
+
+public class NumberClassifier {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        scanner.close();
+    }
+}
+
