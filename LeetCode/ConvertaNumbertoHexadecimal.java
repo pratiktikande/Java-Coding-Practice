3a7453779565059ebae672e531165268e57786ec
unknown <pratiktikande12@gmail.com>
2023-08-31 21:42:38 +0530

    Readme & Hexadecimal

---
+++ LeetCode/ConvertaNumbertoHexadecimal.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+public class ConvertaNumbertoHexadecimal {
+
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        
+        StringBuilder result = new StringBuilder();
+        char[] hexDigits = "0123456789abcdef".toCharArray();
+        
+        while (num != 0) {
+            result.insert(0, hexDigits[num & 0xF]);
+            num >>>= 4; // Unsigned right shift by 4 bits
+        }
+        
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ConvertaNumbertoHexadecimal converter = new ConvertaNumbertoHexadecimal();
+        
+        System.out.println(converter.toHex(26));  // Output: "1a"
+        System.out.println(converter.toHex(-1));  // Output: "ffffffff"
+    }
+}
+
+
+
