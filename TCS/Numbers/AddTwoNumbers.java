6be8e9a02c3991027e93d938f86c98a3a9696725
unknown <pratiktikande12@gmail.com>
2023-09-01 22:43:03 +0530

    Leetcode Problem 258

---
+++ TCS/Numbers/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+package TCS.Numbers;
+
+public class AddTwoNumbers {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers solution = new AddTwoNumbers();
+        
+        int num = 38; // Change this to the desired input
+        int result = solution.addDigits(num);
+        
+        System.out.println("The digital root of " + num + " is: " + result);
+    }
+}
+
