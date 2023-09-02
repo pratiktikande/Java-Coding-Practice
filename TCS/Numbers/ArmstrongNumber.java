5048be58b10e617d5236e410da1d012fc6c75d09
unknown <pratiktikande12@gmail.com>
2023-09-02 23:53:03 +0530

    ArmstrongNumber

---
+++ TCS/Numbers/ArmstrongNumber.java
@@ -0,0 +1,35 @@
+package TCS.Numbers;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        boolean isArmstrong = isArmstrongNumber(number);
+        
+        if (isArmstrong) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+        scanner.close();
+    }
+
+    public static boolean isArmstrongNumber(int num) {
+        int originalNumber = num;
+        int numberOfDigits = String.valueOf(num).length();
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, numberOfDigits);
+            num /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
