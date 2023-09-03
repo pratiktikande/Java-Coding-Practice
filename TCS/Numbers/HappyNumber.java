d3124a7434022ec60b394333a29b2001d56cc369
unknown <pratiktikande12@gmail.com>
2023-09-03 21:16:46 +0530

    HappyNumber

---
+++ TCS/Numbers/HappyNumber.java
@@ -0,0 +1,32 @@
+package TCS.Numbers;
+
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        HappyNumber solution = new HappyNumber();
+        int n = 19;
+        boolean result = solution.isHappy(n);
+        System.out.println("Is " + n + " a Happy Number? " + result);
+    }
+}
+
