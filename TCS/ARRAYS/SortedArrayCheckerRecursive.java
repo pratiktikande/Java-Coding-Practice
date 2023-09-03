f4a93bd720cd4cfbd2d695c1e1bca74d37215fa0
unknown <pratiktikande12@gmail.com>
2023-09-03 21:42:56 +0530

    Recursive Approach

---
+++ TCS/ARRAYS/SortedArrayCheckerRecursive.java
@@ -0,0 +1,23 @@
+package TCS.ARRAYS;
+
+public class SortedArrayCheckerRecursive {
+    //Recursive Approach
+    public static boolean sortedArrayCheckerRecursive(int[] arr, int index) {
+        if (index <= 0) {
+            return true;
+        }
+
+        if (arr[index] < arr[index - 1]) {
+            return false;
+        }
+        // Recursion
+        return sortedArrayCheckerRecursive(arr, index - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int index = arr.length - 1;
+        boolean sorted = sortedArrayCheckerRecursive(arr, index);
+        System.out.println("Is the array sorted (Recursive)? " + sorted);
+    }
+}
