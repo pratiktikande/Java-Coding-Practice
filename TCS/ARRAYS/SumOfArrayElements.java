ca97c48462d09f94a681013f2e7375ced4a48747
unknown <pratiktikande12@gmail.com>
2023-09-03 21:52:56 +0530

    SumOfArrayElements

---
+++ TCS/ARRAYS/SumOfArrayElements.java
@@ -0,0 +1,17 @@
+package TCS.ARRAYS;
+
+public class SumOfArrayElements {
+    public static int sumOfArray(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 6, 9};
+        int result = sumOfArray(nums);
+        System.out.println("Sum of array elements: " + result);
+    }
+}
