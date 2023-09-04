d086b5b639d29cb2ece604a383394c4ce40d68fb
unknown <pratiktikande12@gmail.com>
2023-09-04 23:47:37 +0530

    PascalTriangle

--- LeetCode/PascalTriangle.java
+++ LeetCode/PascalTriangle.java
@@ -2,7 +2,7 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class PascalTriangle {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
---
+++ TCS/ARRAYS/PascalTriangle.java
@@ -0,0 +1,36 @@
+package TCS.ARRAYS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(row);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        PascalTriangle solution = new PascalTriangle();
+        int numRows = 5;
+        List<List<Integer>> result = solution.generate(numRows);
+
+        // Print the generated Pascal's Triangle
+        for (List<Integer> row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
