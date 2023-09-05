769b62f116361e30584278f983c5d03ac5bdb82d
unknown <pratiktikande12@gmail.com>
2023-09-05 23:40:54 +0530

    Leetcode P-217

---
+++ TCS/ARRAYS/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package TCS.ARRAYS;
+
+import java.util.Arrays;
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<=nums.length-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+      return false;
+    }
+    // public static void main(String[] args) {
+    //     int[]nums={1,2,3,1};
+    // }
+}
+
+//www.github.com/pratiktikande
+//@Pratik Tikande
