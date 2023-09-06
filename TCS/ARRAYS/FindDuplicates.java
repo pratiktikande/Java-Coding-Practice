516852b9753876950b28ed618339098522ebfb2a
unknown <pratiktikande12@gmail.com>
2023-09-06 23:21:24 +0530

    FindDuplicate

---
+++ TCS/ARRAYS/FindDuplicates.java
@@ -0,0 +1,22 @@
+package TCS.ARRAYS;
+
+//Find duplicates in O(n) time and O(1) extra space | Set 1
+class FindDuplicate {
+  
+    public static void main(String args[])
+    {
+        int numRay[] = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+  
+        for (int i = 0; i < numRay.length; i++) {
+            numRay[numRay[i] % numRay.length]
+                = numRay[numRay[i] % numRay.length]
+                  + numRay.length;
+        }
+        System.out.println("The repeating elements are : ");
+        for (int i = 0; i < numRay.length; i++) {
+            if (numRay[i] >= numRay.length * 2) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
