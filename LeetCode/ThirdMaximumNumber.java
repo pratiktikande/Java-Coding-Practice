fdab3645195ea078d70448cea9803a998c54d140
unknown <pratiktikande12@gmail.com>
2023-09-07 17:37:08 +0530

    ThirdMaximumNumber

---
+++ LeetCode/ThirdMaximumNumber.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 1;
+        int flag = 0;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] == nums[i - 1]) {
+                flag = flag + 1; // for checking how many times max or second max value repeating
+            } else {
+                count = count + 1; // for getting distinct max value count (until 3rd max value)
+            }
+            if (count == 3) {
+                return nums[n - count - flag]; // n=total value , count = 3, flag = how many times max or second max
+                                               // repeating
+            }
+        }
+        return nums[n - 1];
+    }
+}
---
+++ TCS/ARRAYS/ThirdMaximumNumber.java
@@ -0,0 +1,32 @@
+package TCS.ARRAYS;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ThirdMaximumNumber {
+    public int thirdMax(int[] nums) {
+        Integer[] arr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        int distinctCount = 0;
+        Integer prev = null;
+        
+        for (Integer num : arr) {
+            if (!num.equals(prev)) {
+                distinctCount++;
+                if (distinctCount == 3) {
+                    return num;
+                }
+            }
+            prev = num;
+        }
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        ThirdMaximumNumber solution = new ThirdMaximumNumber();
+        int[] nums = { 3, 1, 2, 4 };
+        int thirdMax = solution.thirdMax(nums);
+        System.out.println("Third Maximum Number: " + thirdMax);
+    }
+}
