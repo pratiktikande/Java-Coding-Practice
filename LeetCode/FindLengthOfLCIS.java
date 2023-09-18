e48d8e25c861198a7ba24de266f98bc1531a1265
unknown <pratiktikande12@gmail.com>
2023-09-19 00:28:25 +0530

    FindLengthOfLCIS

---
+++ LeetCode/FindLengthOfLCIS.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class FindLengthOfLCIS {
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            int len=1,max=0;
+            for(int i=1;i<nums.length;i++)
+            {
+                if(nums[i-1]<nums[i])
+                    len++;
+                else
+                {
+                    max=Math.max(len,max);
+                    len=1;
+                }
+            }
+            return Math.max(max,len);
+        }
+    }
+}
