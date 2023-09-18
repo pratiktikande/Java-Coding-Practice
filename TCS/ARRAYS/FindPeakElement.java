9b2eb5dcfed6de07f0e8e76db500f90fa5fab51a
unknown <pratiktikande12@gmail.com>
2023-09-18 23:50:22 +0530

    FindPeakElement

---
+++ TCS/ARRAYS/FindPeakElement.java
@@ -0,0 +1,18 @@
+package TCS.ARRAYS;
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length== 1)
+            return 0;
+        int max= Integer.MIN_VALUE;
+        int index= Integer.MIN_VALUE;
+        for(int i= 0; i< nums.length; i++){
+            if(max< nums[i]){
+                max= nums[i];
+                index= i;
+            }
+        }
+
+        return index;
+    }
+}
