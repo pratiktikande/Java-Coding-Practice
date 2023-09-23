0d0512ae789ad69319421fa841063c93bfd1d452
unknown <pratiktikande12@gmail.com>
2023-09-23 17:07:49 +0530

    Basic Operations

--- DSA/LinkedList/RemoveLast.java
+++ DSA/LinkedList/RemoveLast.java
@@ -1,6 +1,6 @@
 package DSA.LinkedList;
 
-public class RemoveFirst {
+public class RemoveLast {
 
     public static class Node {
         int data;
@@ -15,6 +15,7 @@ public class RemoveFirst {
 
     public static Node head;
     public static Node tail;
+    public static int size; // Added size to keep track of the linked list size
 
     // We Will Create Methods To
     // add data at the beginning
@@ -25,12 +26,13 @@ public class RemoveFirst {
         // Base case: If the list is empty, set both head and tail to the new node
         if (head == null) {
             head = tail = newNode;
+            size = 1; // Update size
             return;
         }
-
         // Otherwise, link the new node to the current head and update the head
         newNode.next = head;
         head = newNode;
+        size++; // Increment size
     }
 
     // add data at the end
@@ -41,12 +43,14 @@ public class RemoveFirst {
         // Base case: If the list is empty, set both head and tail to the new node
         if (head == null) {
             head = tail = newNode;
+            size = 1; // Update size
             return;
         }
 
         // Otherwise, link the current tail to the new node and update the tail
         tail.next = newNode;
         tail = newNode;
+        size++; // Increment size
     }
 
     // To Print LinkedList
@@ -64,7 +68,7 @@ public class RemoveFirst {
     }
 
     // Add In The Middle
-    public void addmiddle(int index, int data) {
+    public void addMiddle(int index, int data) {
         // Base case
         if (index == 0) {
             addFirst(data);
@@ -80,28 +84,64 @@ public class RemoveFirst {
             temp = temp.next;
             i++;
         }
-        // After getting previous Chnage Linkage
+        // After getting previous, Change Linkage
         newNode.next = temp.next;
         temp.next = newNode;
+        size++; // Increment size
     }
 
     // Remove First LinkedList
     public int removeFirst() {
-        // Base Case If LinkedList Is empty or 1 Then Return Integer.Min_value;
+        // Base Case: If LinkedList Is empty
+        if (head == null) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
         int val = head.data;
         head = head.next;
+        size--; // Decrement size
+        return val;
+    }
+
+    public int removeLast() {
+        // Base Case: If LinkedList Is empty
+        if (head == null) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        // If size is 1, delete head and tail
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        // Remove Last LinkedList
+        // previous = i = size - 2;
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--; // Decrement size
         return val;
     }
 
     public static void main(String[] args) {
-        RemoveFirst list = new RemoveFirst();
+        RemoveLast list = new RemoveLast();
         list.addFirst(1);
         list.addFirst(2);
         list.addLast(3);
         list.addLast(4);
-        list.addmiddle(2, 9);
+        list.addMiddle(2, 9);
         list.printLinkedList();
         list.removeFirst();
+        list.printLinkedList();
+        list.removeLast();
         list.printLinkedList();
     }
 }
