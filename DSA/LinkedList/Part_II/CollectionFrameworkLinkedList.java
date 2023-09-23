4c93d595ec386f133b99eb4ffea1b4b6cf05598d
unknown <pratiktikande12@gmail.com>
2023-09-24 01:46:42 +0530

    Advance LinkedList

--- DSA/LinkedList/Part_II/CollectionFrameworkLinkedList.java
+++ DSA/LinkedList/Part_II/CollectionFrameworkLinkedList.java
@@ -6,12 +6,15 @@ import java.util.LinkedList;
 
 public class CollectionFrameworkLinkedList {
     public static void main(String[] args) {
+
+        //Create LinkList
         LinkedList<Integer> ll = new LinkedList<>();
 
+        //Add elements
         ll.addFirst(1);
         ll.addFirst(2);
         ll.addFirst(3);
-
+        //Print 
         System.out.println(ll);
     }
 }
