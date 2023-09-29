ef7575d0fd80d1734975789fbe3a8e38a9fa1177
unknown <pratiktikande12@gmail.com>
2023-09-29 21:21:52 +0530

    ReverseString

--- TCS/Strings/ReverseString.java
+++ TCS/Strings/ReverseString.java
@@ -1,4 +1,5 @@
 package TCS.Strings;
+
 public class ReverseString {
     public static void main(String[] args) {
         String original = "Hello, World!";
@@ -7,3 +8,21 @@ public class ReverseString {
         System.out.println(result);
     }
 }
+
+
+//Using Loop
+// public class ReverseString {
+//     public static void main(String[] args) {
+//         String original = "Hello, World!";
+//         String reversed = reverseString(original);
+//         System.out.println(reversed);
+//     }
+
+//     public static String reverseString(String input) {
+//         StringBuilder reversed = new StringBuilder();
+//         for (int i = input.length() - 1; i >= 0; i--) {
+//             reversed.append(input.charAt(i));
+//         }
+//         return reversed.toString();
+//     }
+// }
