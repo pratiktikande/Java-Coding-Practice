b678da41d633d98f53034a7a25051f2bf37e2f46
unknown <pratiktikande12@gmail.com>
2023-10-01 23:29:24 +0530

    BubbleSort

---
+++ DSA/Algorithms/BubbleSort.java
@@ -0,0 +1,34 @@
+package DSA.Algorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void Bubblesort(int arr[]){
+        for(int turn=0; turn<arr.length-1; turn++){
+            for(int j=0; j<arr.length-1-turn; j++){
+                if(arr[j] > arr[j+1]){
+                    // swap
+                    int temp= arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    
+//     public static void Printarray(int arr[]){
+//     for(int i=0; i<arr.length; i++){
+//         System.out.print(arr[i] +" ");
+//     }
+//     System.out.println();
+// }
+        public static void main(String[] args) {
+            int arr[]={5,4,1,3,2};
+            Bubblesort(arr);
+            // Printarray(arr);
+            System.out.println(Arrays.toString(arr)); //Direct Statement To Print Array
+        }
+    }
+    
+
+
