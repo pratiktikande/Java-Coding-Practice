22b614279d840c6bd5f794b3f5154a5108fee33c
unknown <pratiktikande12@gmail.com>
2024-03-10 15:19:57 +0530

    ArrayList

---
+++ DSA/ArrayLists/IntroOfSyntax.java
@@ -0,0 +1,12 @@
+package DSA.ArrayLists;
+
+import java.util.ArrayList;
+
+public class IntroOfSyntax {
+    public static void main(String[] args) {
+
+        //Syntax To Create Array List
+        ArrayList<Integer> list =new ArrayList<>();
+        ArrayList<String> string = new ArrayList<>();
+    }
+}
